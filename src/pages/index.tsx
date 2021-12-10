fc13dda095719f3f9666a3dd4a535a4c1bf8cc7c
Jonatas Ferreira <silvajonatas2@gmail.com>
2021-12-09 21:15:50 -0300

    Página Sign In

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,66 @@
-
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <h1>Dashgo</h1>
-  )
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+      >
+      <Flex
+      as="form"
+      width="100%"
+      maxWidth={360}
+      bg="gray.800"
+      p="8"
+      borderRadius={8}
+      flexDir="column"
+      >
+        <Stack spacing={"4"}>
+          <FormControl>
+            <FormLabel htmlFor="email" >E-MAIL</FormLabel>
+
+            <Input
+              id="email"
+              name="email"
+              type="email" 
+              focusBorderColor="pink.500"
+              bgColor={"gray.900"}
+              variant={"filled"}
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size={"lg"}
+              />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password" >PASSWORD</FormLabel>
+
+            <Input
+              id="password"
+              name="password"
+              type="password" 
+              focusBorderColor="pink.500"
+              bgColor={"gray.900"}
+              variant={"filled"}
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size={"lg"}
+              />
+              </FormControl>
+        </Stack>
+        <Button
+          type="submit"
+          marginTop="6"
+          colorScheme="pink"
+          size="lg"
+          >
+           Sign In
+          </Button>
+      </Flex>
+    </Flex>
+  );
 }
