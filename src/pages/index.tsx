4468d51f03f325f30921fc277684d851351160f8
Jonatas Ferreira <silvajonatas2@gmail.com>
2021-12-10 17:30:32 -0300

    Component Input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,65 +1,25 @@
-import { Flex, Input, Button, Stack, FormLabel, FormControl } from "@chakra-ui/react";
+import { Flex, Button, Stack } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
 
-export default function Home() {
+export default function SignIn() {
   return (
-    <Flex
-      w="100vw"
-      h="100vh"
-      align="center"
-      justify="center"
-      >
+    <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
-      as="form"
-      width="100%"
-      maxWidth={360}
-      bg="gray.800"
-      p="8"
-      borderRadius={8}
-      flexDir="column"
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
       >
         <Stack spacing={"4"}>
-          <FormControl>
-            <FormLabel htmlFor="email" >E-MAIL</FormLabel>
-
-            <Input
-              id="email"
-              name="email"
-              type="email" 
-              focusBorderColor="pink.500"
-              bgColor={"gray.900"}
-              variant={"filled"}
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-              size={"lg"}
-              />
-          </FormControl>
-
-          <FormControl>
-            <FormLabel htmlFor="password" >PASSWORD</FormLabel>
-
-            <Input
-              id="password"
-              name="password"
-              type="password" 
-              focusBorderColor="pink.500"
-              bgColor={"gray.900"}
-              variant={"filled"}
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-              size={"lg"}
-              />
-              </FormControl>
+          <Input name="email" type="email" label="E-MAIL" />
+          <Input name="password" type="password" label="PASSWORD" />
         </Stack>
-        <Button
-          type="submit"
-          marginTop="6"
-          colorScheme="pink"
-          size="lg"
-          >
-           Sign In
-          </Button>
+        <Button type="submit" marginTop="6" colorScheme="pink" size="lg">
+          Sign In
+        </Button>
       </Flex>
     </Flex>
   );
