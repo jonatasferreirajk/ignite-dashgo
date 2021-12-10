824c11d022c6bb84397ae52c3d597559e53fef92
Jonatas Ferreira <silvajonatas2@gmail.com>
2021-12-10 18:46:58 -0300

    Finalizando Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri';
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
+import { RiSearchLine, RiNotificationLine, RiUserAddLine } from 'react-icons/ri';
 
 export  function Header(){
   return(
@@ -48,6 +48,37 @@ export  function Header(){
 
           <Icon as={RiSearchLine} fontSize="20" />
         </Flex>
+
+        <Flex
+          align="center"
+          ml="auto"
+        >
+          <HStack
+            spacing="8"
+            mx="8"
+            pr="8"
+            py="1"
+            color="gray.300"
+            borderRightWidth={1}
+            borderColor="gray.700"
+            >
+            <Icon as={RiNotificationLine} font-size="20" />
+            <Icon as={RiUserAddLine} font-size="20" />
+          </HStack>
+
+          <Flex align="center">
+            <Box mr="4" textAlign="right" >
+              <Text>Jonatas Ferreira</Text>
+              <Text color="gray.300" fontSize="small" >
+                silvajonatas2@gmail.com
+              </Text>
+            </Box>
+
+            <Avatar size="md" name="Jonatas Ferreira" 
+              src="https://github.com/jonatasferreirajk.png" />
+          </Flex>
+        </Flex>
+
     </Flex>
   );
 }
